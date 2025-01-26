671094c9c5e59896dd7a3ddc785d84b72554b8cd
Justin Williams <Justintw51@gmail.com>
2025-01-26 13:42:24 -0500

    Added in players and numbers to choose from for rolling

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Dice_game
+{
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int playerRandomNum;
+            int enemyRandomNum;
+
+            Random random = new Random();   
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Press any key to roll the dice.");
+
+                Console.ReadKey();
+
+                playerRandomNum = random.Next(1, 10);
+                Console.WriteLine("John Connor rolled a " + playerRandomNum);
+
+                Console.WriteLine("...");
+                System.Threading.Thread.Sleep(1000);
+
+                enemyRandomNum = random.Next(1, 10);
+                Console.WriteLine("Skynet AI rolled a " + enemyRandomNum);
+            }
+            Console.ReadKey();
+           
+        }
+    }
+}
